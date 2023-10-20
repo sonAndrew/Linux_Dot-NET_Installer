30099a3d58edfc63a21630610ebc0ea4aae2b3c2
testbox2 <andrewz.jackson667@gmail.com>
2023-10-20 11:33:26 -0500

    Created Debian 12 installer script.

---
+++ distro-based-installers/debian-12-installer.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+# This Script installs the .NET SDK or Runtime for Debian Linux.
+
+# Install .NET 7 on Debian 12
+
+# When a Debian version falls out of support, .NET is no longer supported with that version. 
+# However, these instructions may help you to get .NET running on these versions, even though it isn't supported.
+# Install the SDK (which includes the runtime) if you want to develop .NET apps. Or, if you only need to run apps, install the Runtime. 
+# If you're installing the Runtime, we suggest you install the ASP.NET Core Runtime as it includes both .NET and ASP.NET Core runtimes.
+
+# Prompt .NET install function.
+prompt_install_type(){
+    # Prompt which .NET you want to install.
+    echo " "
+    echo "***********************************"
+    echo "*                                 *"
+    echo "*  Which do you want to install?  *"
+    echo "*                                 *"
+    echo "*              $1                *"
+    echo "*              $2            *"
+    echo "*                                 *"
+    echo "***********************************"
+    echo " "
+    echo "* Enter ( $3 ) or ( $4 )."
+}
+
+prompt_install_type SDK Runtime sdk rt
+read install_type
+
+case $install_type in
+    wget https://packages.microsoft.com/config/debian/12/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+    sudo dpkg -i packages-microsoft-prod.deb
+    rm packages-microsoft-prod.deb
+    sdk)
+        prompt_install_type 7.0 6.0 7 6
+        read version
+        case $version in
+            6)
+                sudo apt-get install -y dotnet-sdk-6.0
+            ;;
+            7)
+                sudo apt-get install -y dotnet-sdk-7.0
+            ;;
+            *)
+                printf("A valid input was not entered and now exiting program.")
+            ;;
+        esac
+    ;;
+    rt)
+        prompt_install_type ASP Dot-NET asp net
+        read runtime_type
+
+        case $runtime_type in
+            asp)
+                prompt_install_type 7.0 6.0 7 6
+                read version
+                case $version in
+                    6)
+                        sudo apt-get install -y aspnetcore-runtime-6.0
+                    ;;
+                    7)
+                        sudo apt-get install -y aspnetcore-runtime-7.0
+                    ;;
+                    *)
+                        printf("A valid input was not entered and now exiting program.")
+                    ;;
+                esac
+            ;;
+            net)
+                prompt_install_type 7.0 6.0 7 6
+                read version
+                case $version in
+                    6)
+                        sudo apt-get install -y dotnet-runtime-6.0
+                    ;;
+                    7)
+                        sudo apt-get install -y dotnet-runtime-7.0
+                    ;;
+                    *)
+                        printf("A valid input was not entered and now exiting program.")
+                    ;;
+                esac
+            ;;
+            *)
+                printf("A valid input was not entered and now exiting program.")
+            ;;
+        esac
+    ;;
+    *)
+        printf("A valid input was not entered and now exiting program.")
+    ;;
+esac
